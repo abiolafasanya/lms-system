76bf1b592837affd19c84a15428d0c4febf9e799
fastbeetech <hezekiahfasanya@gmail.com>
2023-01-04 18:00:32 +0100

    remove text.html and changed nextauth pages

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -73,7 +73,7 @@ const options: NextAuthOptions = {
   ],
 
   pages: {
-    signIn: '/auth/signin', // Displays signin buttons
+    signIn: '/auth/login', // Displays signin buttons
     // signOut: '/auth/signout', // Displays form with sign out button
     // error: '/auth/error', // Error code passed in query string as ?error=
     // verifyRequest: '/auth/verify-request', // Used for check email page
